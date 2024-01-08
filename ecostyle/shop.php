2b30ca10a04b1c8debe3f369779d2ed4b62d6149
Vanessa Innerbichler <vanessa.innerbichler@gmail.com>
2024-01-08 14:44:25 +0100

    add shop.

---
+++ ecostyle/shop.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="de">
+
+<head>
+    <meta charset="UTF-8">
+    <title>EcoStyle Goller</title>
+    <?php include 'head.php'; ?>
+</head>
+
+<body>
+    <header class="mb-5">
+        <?php include 'menu.php'; ?>
+
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12 d-flex justify-content-center">
+                    <button class="btn_shop me-3">alles anzeigen</button>
+                    <button class="btn_shop me-3">Kleidung</button>
+                    <button class="btn_shop me-3">Accessoires</button>
+                    <button class="btn_shop">Taschen</button>
+                </div>
+                <div class="col-md-12 d-flex justify-content-center mt-4">
+                    <p>Sortieren nach <i class="fas fa-sliders-h ms-2"></i></p>
+                </div>
+            </div>
+        </div>
+
+    </header>
+
+    <?php include 'footer.php'; ?>
+
+    <script src="js/bootstrap.min.js"></script>
+
+</body>
+
+</html>
