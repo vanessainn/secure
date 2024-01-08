596c1e2640652d0c3550adc27a76df72a3334a8a
Vanessa Innerbichler <vanessa.innerbichler@gmail.com>
2024-01-08 15:12:00 +0100

    add artikel.

---
+++ ecostyle/artikel.php
@@ -0,0 +1,5 @@
+<?php 
+    $artikel = ["Recycelte Denim-Jeans", "Bio-Woll-Beanie", "Fair Trade Baumwollkleid", "Recycelte Brillenfassung", "Upcycled Lederhandtasche", "Goldene Ohrringe", "Bio-Leinenhemd", "Handgewebter Wollschal", "Bio-Baumwoll-T-Shirt", "Bambus-Socken"];
+    $beschreibung = ["Stilvoll, umweltbewusst, zeitlos", "Warm, bio-zertifiziert, zeitlos", "Ethik, Stil, zeitlos", "Nachhaltig stilvoll aus recyceltem Holz", "Stilvoll, einzigartig, aus recyceltem Leder", "Handgefertigt, recyceltes Gold, zeitlose Eleganz", "Leicht, atmungsaktiv, ökologisch", "Einzigartiges Muster, kuschelig warm", "Weich, atmungsaktiv, umweltfreundlich", "Atmungsaktiv, nachhaltig, bequem"];
+    $preis = [49.99, 19.99, 59.99, 139.99, 159.99, 29.99, 59.99, 19.99, 29.99, 18.99];
+?>
--- ecostyle/shop.php
+++ ecostyle/shop.php
@@ -27,9 +27,65 @@
 
     </header>
 
+    <main>
+    <?php include 'artikel.php'; ?>
+        <div class="container">
+            <div class="row">
+                <div class="col-md-5">
+                    <h6>Artikel</h6>
+                </div>
+                <div class="col-md-3">
+                    <h6>Einzelpreis</h6>
+                </div>
+                <div class="col-md-3">
+                    <h6>Anzahl</h6>
+                </div>
+                <div class="col-md-1">
+                    <h6>Summe</h6>
+                </div>
+                <hr class="mt-3">
+            </div>
+        </div>
+        <div class="container">
+            <form id="bestellform">
+                <?php for ($i = 0; $i < count($artikel); $i++) {
+                ?>
+                <div class="row">
+                    <div class="col-md-5 mb-3">
+                        <?php echo $artikel[$i];?><br>
+                        <?php echo $beschreibung[$i];?>
+                    </div>
+                    <div class="col-md-3">
+                        <p id='<?php echo 'artikelpreis_' . $i; ?>'> <?php echo number_format($preis[$i], 2);?></p>
+                    </div>
+                    <div class="col-md-3">
+                        <input id='<?php echo 'artikelanzahl_' . $i; ?>'  type="number" value='0' min='0'>
+                    </div>
+                    <div class="col-md-1">
+                        <p id='<?php echo 'artikelsumme_' . $i; ?>' class="d-flex justify-content-end">0.00</p>
+                    </div>
+                    <hr>
+                </div>
+                <?php } ?>
+                <div class="row">
+                    <div class="offset-md-9 col-md-3">
+                        <p id="gesamtsumme" class="justify-content-end d-flex fw-bold">0.00</p>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="offset-md-9 col-md-3">
+                        <button type="submit" class="btn btn-primary btn-larage">Bestellen</button>
+                    </div>
+                </div>
+                <input value='<?php echo count($artikel); ?>' id="anzahlArtikel" hidden>
+            </form>
+        </div>
+    </main>
+
     <?php include 'footer.php'; ?>
 
     <script src="js/bootstrap.min.js"></script>
+    <script src="js/ecostyle.js"></script>
 
 </body>
 
